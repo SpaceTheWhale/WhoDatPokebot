6b7302a484c0495f9e0b0cbf9fb2e9bc64156d44
SpaceTheWhale <70277754+SpaceTheWhale@users.noreply.github.com>
2021-11-22 15:19:28 -0500

    First Commit
    Sprites and Previous Code

---
+++ WhoDatPokebot/Pbot.js
@@ -0,0 +1,36 @@
+//Who's That Pokemon Discord Bot
+
+// Require the necessary discord.js classes
+const { Client, Intents } = require('discord.js');
+require('dotenv').config();
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+//Set .env vars
+const serverID = process.env.SERVERID;
+const channelID = process.env.CHANNELID;
+var rP;
+
+// Login to Discord with your client's token
+client.login(process.env.token);
+
+// When the client is ready, run this code (only once)
+client.once('ready', botReady);
+
+function botReady() {
+    console.log('Bot is ready!'); 
+}
+
+
+
+client.on('interactionCreate', async (interaction) => {
+    if (interaction.commandName === 'whodatpokemon') {
+        rP = Math.floor(Math.random() * (152 - 1) + 1);
+        await interaction.reply("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+rP+".png");
+    }
+});
+
+
+
+
